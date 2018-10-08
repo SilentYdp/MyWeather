03c5c8785afaa68fc3fc5027806a29a88a578bd2
SilentYdp <yedepeng@pku.edu.cn>
2018-10-08 22:37:55 +0800

    local weather interface

---
+++ app/src/main/java/cn/edu/yedepengpku/myweather/MainActivity.java
@@ -0,0 +1,12 @@
+package cn.edu.yedepengpku.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
