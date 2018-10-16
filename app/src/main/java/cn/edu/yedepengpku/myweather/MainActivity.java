528ebccda62b780c67205e3dd91ec734284b6ff8
SilentYdp <yedepeng@pku.edu.cn>
2018-10-16 16:09:07 +0800

    add internet

--- app/src/main/java/cn/edu/yedepengpku/myweather/MainActivity.java
+++ app/src/main/java/cn/edu/yedepengpku/myweather/MainActivity.java
@@ -1,12 +1,92 @@
 package cn.edu.yedepengpku.myweather;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 
-public class MainActivity extends Activity {
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import cn.edu.pku.yedepeng.util.NetUtil;
+
+public class MainActivity extends Activity implements View.OnClickListener
+{
+    private ImageView mUpdateBtn;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        mUpdateBtn=(ImageView)findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+        if(NetUtil.getNeworkState(this)!=NetUtil.NETWORK_NONE){
+            Log.d("myWeather","网络ok");
+            Toast.makeText(MainActivity.this,"网络ok！",Toast.LENGTH_LONG).show();
+        }else {
+            Log.d("myWeather","网络未连接");
+            Toast.makeText(MainActivity.this,"网络未连接",Toast.LENGTH_LONG).show();
+        }
     }
+    public void onClick(View view) {
+        if (view.getId() == R.id.title_update_btn) {
+            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_city_code", "101010100");
+            Log.d("myWeather", cityCode);
+
+            if (NetUtil.getNeworkState(this) != NetUtil.NETWORK_NONE) {
+                Log.d("myWeather", "网络ok");
+                queryWeatherCode(cityCode);
+            } else {
+                Log.d("myWeather", "网络未连接");
+                Toast.makeText(MainActivity.this, "网络未连接", Toast.LENGTH_LONG).show();
+                ;
+            }
+        }
+    }
+
+    private void queryWeatherCode(final String cityCode){
+        final String address="http://wthrcdn.etouch.cn/WeatherApi?citykey="+cityCode;
+        Log.d("myWeather",address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con=null;
+                try{
+                    URL url=new URL(address);
+                    con=(HttpURLConnection)url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setConnectTimeout(8000);
+                    con.setReadTimeout(8000);
+                    InputStream in=con.getInputStream();
+                    BufferedReader reader=new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response=new StringBuilder();
+                    String str;
+                    while ((str=reader.readLine())!=null){
+                        response.append(str);
+                        Log.d("myWeather",str);
+                    }
+                    String responseStr=response.toString();
+                    Log.d("myWeather",responseStr);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(con!=null){
+                        con.disconnect();
+                    }
+                }
+
+            }
+        }).start();
+    }
+
 }
